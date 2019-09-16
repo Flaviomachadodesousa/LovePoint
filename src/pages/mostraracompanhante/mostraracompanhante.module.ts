1ddb2f45d15a080e9d4ea7e6723004126f28fbb1
Flavio <flavio.sos.machado@gmail.com>
2019-09-16 08:39:06 -0300

    Create mostraracompanhante.module.ts

---
+++ src/pages/mostraracompanhante/mostraracompanhante.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { MostraracompanhantePage } from './mostraracompanhante';
+
+@NgModule({
+  declarations: [
+    MostraracompanhantePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(MostraracompanhantePage),
+  ],
+})
+export class MostraracompanhantePageModule {}
