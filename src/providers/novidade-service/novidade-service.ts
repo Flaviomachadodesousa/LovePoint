2a772589326dcbf359e4b8db0d0472cd2032c5ed
Flávio Machado de Sousa <34101472+Flaviomachadodesousa@users.noreply.github.com>
2019-12-19 20:35:30 -0300

    Update novidade-service.ts

--- src/providers/novidade-service/novidade-service.ts
+++ src/providers/novidade-service/novidade-service.ts
@@ -7,14 +7,14 @@ export class Novidadesservice {
 
   public items: any;
   url = "https://jsonplaceholder.typicode.com/photos";
-  urlflavio = "https://api.myjson.com/bins/k8bjp";
+  Novidade = "https://acompanhantes-59869.firebaseio.com/Novidades.json";
   urlflavio1 = "https://testeappjson.000webhostapp.com/test.json";
 
   constructor(public http: HttpClient) {
     }
     getUsers() {
       return new Promise(resolve => {
-        this.http.get(this.urlflavio).subscribe(data => {
+        this.http.get(this.Novidade).subscribe(data => {
           resolve(data);
         }, err => {
           console.log(err);
