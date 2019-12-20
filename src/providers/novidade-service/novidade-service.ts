eba45d6379542a0dcbfff85865b1d7e668c61e6d
Flavio <flavio.sos.machado@gmail.com>
2019-12-20 14:18:20 -0300

    Update novidade-service.ts

--- src/providers/novidade-service/novidade-service.ts
+++ src/providers/novidade-service/novidade-service.ts
@@ -6,10 +6,8 @@ import { HttpClient } from '@angular/common/http';
 export class Novidadesservice {
 
   public items: any;
-  url = "https://jsonplaceholder.typicode.com/photos";
   Novidade = "https://acompanhantes-59869.firebaseio.com/Novidades.json";
-  urlflavio1 = "https://testeappjson.000webhostapp.com/test.json";
-
+  
   constructor(public http: HttpClient) {
     }
     getUsers() {
