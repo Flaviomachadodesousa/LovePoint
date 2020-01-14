f0c51529394476a3449036d35fb40717a4521832
Flavio <flavio.sos.machado@gmail.com>
2020-01-14 15:44:52 -0300

    Update mail.php

--- WebSite/mail.php
+++ WebSite/mail.php
@@ -1,20 +1,20 @@
 <?php
 
 #Receive user input
-$name = $_POST['name'];
-$email = $_POST['email'];
-$telefone = $_POST['telefone'];
-$mensagem = $_POST['mensagem'];
+if(isset($_POST['submit'])){
+	$_name = $_POST['name'];
+	$_email = $_POST['email'];
+	$_telefone = $_POST['telefone'];
+	$_mensagem = $_POST['mensagem'];
 
 #Send email
-$from = "flavio.sos.machado@gmail.com";
-$to = "flavio.sos.machado@gmail.com";
-$subject = "Site Acompanhants";
-$headers = "De:". $from;
+	$from = "flavio.sos.machado@gmail.com";
+	$to = "flavio@adamos.com.br";
+	$subject = "Site Acompanhants";
+	$headers = "De:". $from;
 
-mail($to, $subject, $name, $email, $telefone, $mensagem, $headers);
+	mail($to, $subject, $_name, $_email, $_telefone, $_mensagem, $headers);
 
-echo "A mensagem de e-mail foi enviada.";
-
-
+	echo "A mensagem de e-mail foi enviada.";
+}
 ?>
