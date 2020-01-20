7fc38d0cc21a48ec8b4eb4533d3d38f23171791e
Flavio <flavio.sos.machado@gmail.com>
2020-01-20 12:16:23 -0300

    Create indisponivel.module.ts

---
+++ src/pages/indisponivel/indisponivel.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { IndisponivelPage } from './indisponivel';
+
+@NgModule({
+  declarations: [
+    IndisponivelPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(IndisponivelPage),
+  ],
+})
+export class IndisponivelPageModule {}
